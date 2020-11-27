a3f997a48576ed72bdfe159487c2d811b8fc80db
Roshan Padal <rdpadal@gmail.com>
2020-11-27 23:47:57 +0530

    main commit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -33,7 +33,7 @@ function TodoForm({ addTodo }) {
         value={todo.task}
         onChange={handleTaskInputChange}
       />
-      <Button type="submit">Click to submit</Button>
+      <Button type="submit">Submit</Button>
     </form>
   );
 }
